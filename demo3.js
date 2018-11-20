6815cccc76e8c7bd078abcc51c6c18d58f9560cc
tsinn <timothysinnott@hotmail.com>
2018-11-19 21:25:01 -0500

    Carto Positron basemap

--- demo3.js
+++ demo3.js
@@ -4,6 +4,12 @@ function init() {
         minZoom:1,
         maxZoom:8,
     }).setView([39.8283,-98.5795],4);
+
+    var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+     subdomains: 'abcd',
+     maxZoom: 19
+    });
     
     // Define a style
     var routeStyle = {
